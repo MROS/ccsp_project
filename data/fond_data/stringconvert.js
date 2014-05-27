5b374644ae7f1c89dfef6e5dd503c4e0034398ed
JeremyShih <joremy628@hotmail.com>
2014-05-28 00:04:31 +0800

    現有里資料title更改

--- data/fond_data/stringconvert.js
+++ data/fond_data/stringconvert.js
@@ -1,10 +1,42 @@
 var fs = require('fs');
 
-var villagelist = JSON.parse(fs.readFileSync('Xinyidistfonddata.json', 'utf8'));
-// var villagelist = obj;
-// var villagelist = JSON.parse(stri);
-
-fs.writeFile('信義區經費資料.json', JSON.stringify(villagelist, null, 4), function(err) {
+var villagelist = JSON.parse(fs.readFileSync('Beitoudistfonddata.json', 'utf8'));
+fs.writeFile('北投區經費資料.json', JSON.stringify(villagelist, null, 4), function(err) {
 	if (err) throw err;
 	console.log('It\'s saved!');
 });
+var villagelist = JSON.parse(fs.readFileSync('Daandistfonddata.json', 'utf8'));
+fs.writeFile('大安區經費資料.json', JSON.stringify(villagelist, null, 4), function(err) {
+	if (err) throw err;
+	console.log('It\'s saved!');
+});
+var villagelist = JSON.parse(fs.readFileSync('Nangangdistfonddata.json', 'utf8'));
+fs.writeFile('南港區經費資料.json', JSON.stringify(villagelist, null, 4), function(err) {
+	if (err) throw err;
+	console.log('It\'s saved!');
+});
+var villagelist = JSON.parse(fs.readFileSync('Songshandistfonddata.json', 'utf8'));
+fs.writeFile('松山區經費資料.json', JSON.stringify(villagelist, null, 4), function(err) {
+	if (err) throw err;
+	console.log('It\'s saved!');
+});
+var villagelist = JSON.parse(fs.readFileSync('Beitoudistfonddata.json', 'utf8'));
+fs.writeFile('北投區經費資料.json', JSON.stringify(villagelist, null, 4), function(err) {
+	if (err) throw err;
+	console.log('It\'s saved!');
+});
+var villagelist = JSON.parse(fs.readFileSync('Wanhuadistfonddata.json', 'utf8'));
+fs.writeFile('萬華區經費資料.json', JSON.stringify(villagelist, null, 4), function(err) {
+	if (err) throw err;
+	console.log('It\'s saved!');
+});// Wanhuadistfonddata.json
+var villagelist = JSON.parse(fs.readFileSync('Xinyidistfonddata.json', 'utf8'));
+fs.writeFile('信義區經費資料.json', JSON.stringify(villagelist, null, 4), function(err) {
+	if (err) throw err;
+	console.log('It\'s saved!');
+});// Xinyidistfonddata.json
+var villagelist = JSON.parse(fs.readFileSync('Zhongzhengdistfonddata.json', 'utf8'));
+fs.writeFile('中正區經費資料.json', JSON.stringify(villagelist, null, 4), function(err) {
+	if (err) throw err;
+	console.log('It\'s saved!');
+});// Zhongzhengdistfonddata.json
