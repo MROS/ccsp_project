9620fcb16785bdb556f17db06b9f297aba071166
MROS <yc1043@gmail.com>
2014-06-05 12:19:24 +0800

    add city, dist info

--- database/leader_retrive.js
+++ database/leader_retrive.js
@@ -9,7 +9,7 @@ leader_data = {};
 // console.log(files[i]);
 var data = fs.readFileSync('data/里長資料/里長名單.json');
 var leaders = JSON.parse(data);
-for (var j = 0; j < leaders.length; j++) { 
+for (var j = 0; j < leaders.length; j++) {
 	// console.log(villas[j].Villa);
 	leaders[j].villa = leaders[j].villa.slice(0, 2);
 	leader_data[leaders[j].villa] = leaders[j];
